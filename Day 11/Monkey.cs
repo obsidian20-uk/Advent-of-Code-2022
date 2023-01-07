f554f48fc5bca1a7a05f78663bbab910d36658a6
Paul Simmons <paul@simmonsonline.me.uk>
2023-01-07 10:15:03 +0000

    Day 11

---
+++ Day 11/Monkey.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_11
+{
+    internal class Monkey
+    {
+        public Monkey(string monkeytext)
+        {
+            Regex regex = new Regex("monkey .*:starting items: (.*)operation: new = (.*)test: divisible by (.*)if true: throw to monkey (.*)if false: throw to monkey (.*)");
+            var processedmonkeytext = regex.Match(monkeytext.ToLower());
+            foreach (var item in processedmonkeytext.Groups[1].Value.Split(", "))
+            {
+                if (int.TryParse(item, out int val1))
+                {
+                    Items.Enqueue(val1);
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid Starting item: {item}");
+                }
+            }
+            var Operation = processedmonkeytext.Groups[2].Value.Split(" ");
+            Operator = Operation[1];
+            OperationValue = Operation[2];
+
+            if (int.TryParse(processedmonkeytext.Groups[3].Value, out int val3))
+            {
+                TestCondition = val3;
+            }
+            else
+            {
+                Console.WriteLine($"Invalid Test Condition {processedmonkeytext.Groups[3].Value}");
+            }
+
+            if (int.TryParse(processedmonkeytext.Groups[4].Value, out int val4))
+            {
+                TrueMonkey = val4;
+            }
+            else
+            {
+                Console.WriteLine($"Invalid True Monkey Value {processedmonkeytext.Groups[4].Value}");
+            }
+
+            if (int.TryParse(processedmonkeytext.Groups[5].Value, out int val5))
+            {
+                FalseMonkey = val5;
+            }
+            else
+            {
+                Console.WriteLine($"Invalid False Monkey Value {processedmonkeytext.Groups[5].Value}");
+            }
+
+        }
+        public Queue<long> Items { get; set; } = new Queue<long>();
+        public string Operator { get; set; } = "";
+        public string OperationValue { get; set; }
+        public int TestCondition { get; set; }
+        public int TrueMonkey { get; set; }
+        public int FalseMonkey { get; set; }
+        public long ItemsInspected { get; set; } = 0;
+
+        public Tuple<int, long> InspectAndThrowItem(int roundNum, int moduloFactor)
+        {
+            int destination;
+            var item = Items.Dequeue();
+            long Operand;
+            if (OperationValue == "old")
+            {
+                Operand = item;
+            }
+            else
+            {
+                Operand = int.Parse(OperationValue);
+            }
+            switch (Operator)
+            {
+                case "*":
+                    item = item * Operand;
+                    break;
+                case "+":
+                    item = item + Operand;
+                    break;
+            }
+
+            //if (roundNum < 20)
+            //{
+            //    item = item / 3;
+            //}
+            
+            item %= moduloFactor;
+
+            if (item % TestCondition == 0)
+            {
+                destination = TrueMonkey;
+            }
+            else
+            {
+                destination = FalseMonkey;
+            }
+            ItemsInspected++;
+            return new Tuple<int, long>(destination, item);
+        }
+    }
+}
--- Day 11/Program.cs
+++ Day 11/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace Day_11
+﻿using System.Data.SqlTypes;
+
+namespace Day_11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var Monkeys = new List<Monkey>();
             string[] lines = System.IO.File.ReadAllLines(@"PuzzleInput.txt");
+            foreach (var monkeytext in InputProcessor(lines.Where(l => l != "").ToList()))
+            {
+                Monkeys.Add(new Monkey(monkeytext));
+            }
+
+            var factor = Monkeys.Aggregate(1, (c, m) => c * m.TestCondition);
+
+            var test = 17 % 9699690;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                foreach (var monkey in Monkeys)
+                {
+                    while (monkey.Items.Count() != 0)
+                    {
+                        var item = monkey.InspectAndThrowItem(i, factor);
+                        Monkeys[item.Item1].Items.Enqueue(item.Item2);
+                    }
+                }
+            }
+
+            var top2Monkeys = Monkeys.OrderByDescending(m => m.ItemsInspected).Take(2).ToArray();
+
+            foreach (var item in top2Monkeys)
+            {
+                Console.WriteLine(item.ItemsInspected);
+            }
+
+            var MonkeyBusiness = top2Monkeys[0].ItemsInspected * top2Monkeys[1].ItemsInspected;
+
+            Console.WriteLine($"Monkey Business: {MonkeyBusiness}");
+
+            Console.ReadLine();
+        }
+
+        public static List<string> InputProcessor(List<string> input)
+        {
+            List<string> output = new List<string>();
+            var outputstring = "";
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] != "")
+                {
+                    outputstring += input[i].Trim();
+                }
+                if ((i + 1) % 6 == 0)
+                {
+                    output.Add(outputstring);
+                    outputstring = "";
+                }
+            }
+            return output;
         }
     }
 }
